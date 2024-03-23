5362b1dfc9b3b56dc7f6d8b082ec9b9a879dcc0e
Rafael Freitas <raffreitasx@outlook.com>
2024-03-23 19:55:03 -0300

    feat: add user and navigations properties

--- GymManager.Core/Entities/CheckIn.cs
+++ GymManager.Core/Entities/CheckIn.cs
@@ -3,14 +3,19 @@ public class CheckIn : BaseEntity
 {
     protected CheckIn() { }
 
-    public CheckIn(Gym gym)
+    public CheckIn(Gym gym, User user)
     {
         Gym = gym;
+        User = user;
         CreatedAt = DateTime.UtcNow;
     }
 
     public DateTime CreatedAt { get; private set; }
     public DateTime? ValidatedAt { get; private set; }
 
+    public Guid GymId { get; private set; }
     public Gym Gym { get; private set; } = null!;
+
+    public Guid UserId { get; private set; }
+    public User User { get; private set; } = null!;
 }
