ae1199fd03d2e05b1e3f87191b8e78706bfe7fc0
Rafael Freitas <raffreitasx@outlook.com>
2024-03-23 19:55:25 -0300

    feat: add configuration

---
+++ GymManager.Core/Configuration.cs
@@ -0,0 +1,10 @@
+﻿namespace GymManager.Core;
+public static class Configuration
+{
+    public static DatabaseConfiguration Database { get; set; } = new();
+
+    public class DatabaseConfiguration
+    {
+        public string ConnectinString { get; set; } = string.Empty;
+    }
+}
