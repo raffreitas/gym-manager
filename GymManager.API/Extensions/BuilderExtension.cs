ec18f30ae27c13da4ad8cf057914ae56c7247f6c
Rafael Freitas <raffreitasx@outlook.com>
2024-03-24 12:05:00 -0300

    feat: add swagger authentication condig

---
+++ GymManager.API/Extensions/BuilderExtension.cs
@@ -0,0 +1,53 @@
+﻿using GymManager.Core;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+namespace GymManager.API.Extensions;
+
+public static class BuilderExtension
+{
+    public static void AddConfiguration(this WebApplicationBuilder builder)
+    {
+        Configuration.Database.ConnectinString = builder.Configuration
+                                                    .GetConnectionString("DefaultConnection") ?? string.Empty;
+
+        var secrets = new Configuration.SecretsConfiguration();
+        builder.Configuration.GetSection("Secrets").Bind(secrets);
+        Configuration.Secrets = secrets;
+    }
+
+    public static void AddSwagger(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "GymManager.API", Version = "v1" });
+
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "JWT Authorization header using Bearer scheme",
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey,
+                BearerFormat = "JWT",
+                Scheme = "Bearer",
+            });
+
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
+        });
+    }
+}
