737cf5e92a6181bce65a9af428dec88bd3248c93
Rafael Freitas <raffreitasx@outlook.com>
2024-03-23 23:50:44 -0300

    feat: add implementation HashPassword

---
+++ GymManager.Infrastructure/AuthServices/AuthService.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+using GymManager.Core.Services;
+
+namespace GymManager.Infrastructure.AuthServices;
+public class AuthService : IAuthService
+{
+    public string HashPassword(string plainPassword)
+    {
+        byte[] bytes = SHA256.HashData(Encoding.UTF8.GetBytes(plainPassword));
+
+        StringBuilder sb = new();
+        for (int i = 0; i < bytes.Length; i++)
+        {
+            sb.Append(bytes[i].ToString("x2"));
+        }
+
+        return sb.ToString();
+    }
+}
