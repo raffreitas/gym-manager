8ec61ef8462a47ad0c102e3c66b7dccdc96f6fe9
Rafael Freitas <raffreitasx@outlook.com>
2024-03-24 00:13:02 -0300

    feat: add usr controller

---
+++ GymManager.API/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using GymManager.Application.Commands.RegisterUser;
+
+using MediatR;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymManager.API.Controllers;
+
+[Route("/api/users")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    public UserController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost("/register")]
+    public async Task<IActionResult> Register(RegisterUserCommand registerUserCommand)
+    {
+        await _mediator.Send(registerUserCommand);
+
+        return Created();
+    }
+
+}
